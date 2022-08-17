15ade227bce4a6c17b897571e18ef0e7be67e06f
muazkhaledawaja <31365698+muazkhaledawaja@users.noreply.github.com>
2022-08-17 23:19:37 +0300

    make it better

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 
 
 
-const MongoStore =  require('connect-mongodb-session')(session);
+const MongoStore =  require('connect-mongodb')(session);
 
 
 const userRoutes = require('./routes/users');
